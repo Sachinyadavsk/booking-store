afd2bcd84e8e93a76cd9f3d372efd1014e91e992
Sachinyadavsk <sachinsk4832499@gmail.com>
2025-08-17 17:18:21 +0530

    browers error

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <div className="dark:bg-slate-900 dark:text-white">
                 <App />
             </div>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 )
